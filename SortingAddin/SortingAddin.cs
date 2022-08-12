d55263a03f4b76f52d654a917cb63c688a47e444
Emin Hasanov <emin.hasanov.90@outlook.com>
2022-08-12 09:52:18 +0400

    Unused usings deleted

--- SortingAddin/SortingAddin.cs
+++ SortingAddin/SortingAddin.cs
@@ -1,10 +1,5 @@
 ﻿using Aveva.ApplicationFramework;
 using Aveva.ApplicationFramework.Presentation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortingAddin
 {
